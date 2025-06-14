e89a45f625254c959d0df77626c145820fcc1a88
Hiryxx <paolo.ricci042@gmail.com>
2025-06-14 18:42:56 +0200

    updates to cart having max count as stock, updates to css and moved some functions to stock script

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,5 @@
+let createPageChangeEvent = (page) => new CustomEvent('pageChanged', {detail: {page}});
+
 /**
  * Get the function to call after loading the page
  * @param page Page name without url (e.g. "home" for "/pages/home.html")
@@ -18,8 +20,6 @@ const getPageFunction = (page) => {
     }
 }
 
-const createPageChangeEvent = (page) => new CustomEvent('pageChanged', { detail: { page } });
-
 
 const pageId = "current-page"
 const router = new Router("home", "pages/")
@@ -27,7 +27,7 @@ const router = new Router("home", "pages/")
 
 document.addEventListener('DOMContentLoaded', function () {
     const lastPage = localStorage.getItem("currentPage")
-    if(!lastPage) {
+    if (!lastPage) {
         localStorage.setItem("currentPage", "home")
     }
     const res = checkUserAuth()
@@ -40,15 +40,15 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             return res.json();
         }).then(user => {
-            if(user) {
+            if (user) {
                 console.log("User found ", user)
                 UserState.seUserInfo(user)
             }
-        }).finally(()=>{
+        }).finally(() => {
             console.log("Loading page ", lastPage)
             document.dispatchEvent(createPageChangeEvent(lastPage))
         })
-    } else{
+    } else {
         document.dispatchEvent(createPageChangeEvent(lastPage))
     }
 });
@@ -66,7 +66,6 @@ document.addEventListener('pageChanged', function (e) {
 });
 
 
-
 const extractHtml = (htmlUrl, elementId, callAfter) => {
     fetch(htmlUrl)
         .then(response => response.text())
@@ -80,163 +79,6 @@ const extractHtml = (htmlUrl, elementId, callAfter) => {
         });
 }
 
-// todo check if correct and add quantity
-const loadCartPage = () => {
-    let cartItems = CartState.getCartItems() // at least always []
-    const itemToAdd = CartState.getItemToAdd()
-
-    const storedCartItems = JSON.parse(localStorage.getItem("cartItems"))
-
-    if (storedCartItems && cartItems.length === 0 && !itemToAdd)
-        cartItems.push(...storedCartItems)
-
-    if (itemToAdd) {
-        // add the item to the cart
-        console.log("Adding item to cart: ", itemToAdd)
-        // check if the item is already in the cart
-        const existingItemIndex = cartItems.findIndex(item => item.product_id === itemToAdd.product_id)
-        if (existingItemIndex !== -1) {
-            // if it is, increase the quantity
-            cartItems[existingItemIndex].quantity += itemToAdd.quantity
-        } else {
-            cartItems.push(itemToAdd)
-        }
-
-        CartState.setCartItems(cartItems)
-        localStorage.setItem("cartItems", JSON.stringify(cartItems))
-        CartState.setItemToAdd(null) // reset item to add
-    }
-
-        // getting them from local storage
-
-    console.log("Loading cart items from local storage", storedCartItems)
-
-    const cartDiv = document.getElementById("cart-items")
-
-
-
-    if (cartItems.length === 0) {
-        cartDiv.innerHTML = `
-        <div class="cart-empty">
-            <p>
-                Your cart is empty
-            </p>
-        </div>
-        `
-    }
-
-    for(let prod of cartItems){
-        if (!prod || !prod.product_id) {
-            continue
-        }
-        cartDiv.innerHTML += `
-           <div class="cart-item">
-            <div id="cart-image">
-                <img src="http://localhost:900/images?product_id=${prod.product_id}" alt="">
-            </div>
-            <div class="cart-info">
-                <div class="cart-title">
-                     ${prod.name}
-                </div>
-                <div class="cart-price">
-                    $${prod.price}
-                </div>
-                <div onclick="removeProdFromCart(${prod.product_id})" class="cart-remove">
-                    Remove
-                </div>
-            </div>
-            <div class="cart-quantity">
-                <div class="quantity-minus" onclick="modifyQuantity(${prod.product_id}, -1)">
-                 -
-                </div>
-                <p>
-                    ${prod.quantity}
-                </p>
-                <div class="quantity-plus" onclick="modifyQuantity(${prod.product_id}, 1)">
-                 +
-                </div>
-            </div>
-        </div>
-        `
-    }
-}
-
-let modifyQuantity = (productId, quantity) => {
-    let cartItems = localStorage.getItem("cartItems")
-    if (!cartItems) {
-        console.error("No cart items found in local storage")
-        return
-    }
-
-    cartItems = JSON.parse(cartItems)
-    const itemIndex = cartItems.findIndex(item => item.product_id === productId)
-
-    if (itemIndex === -1) {
-        console.error("Product not found in cart")
-        return
-    }
-
-    // Modify the quantity
-    if (quantity < 0 && cartItems[itemIndex].quantity <= 1) {
-        console.warn("Cannot reduce quantity below 1")
-        return
-    }
-    // ensure maximum quantity is not exceeded. Max is 10
-
-    if (quantity > 0 && cartItems[itemIndex].quantity >= 10) {
-        console.warn("Cannot increase quantity above 10")
-        return
-    }
-
-    cartItems[itemIndex].quantity += quantity
-
-    CartState.setCartItems(cartItems)
-    localStorage.setItem("cartItems", JSON.stringify(cartItems))
-
-    document.dispatchEvent(createPageChangeEvent("shopping_cart"));
-}
-
-// todo remove quantity from cart
-let removeProdFromCart = (productId) => {
-    let cartItems = localStorage.getItem("cartItems")
-    if (!cartItems) {
-        console.error("No cart items found in local storage")
-        return
-    }
-
-    cartItems = JSON.parse(cartItems)
-    const newCartItems = cartItems.filter(item => item.product_id !== productId)
-    CartState.setCartItems(newCartItems)
-    localStorage.setItem("cartItems", JSON.stringify(newCartItems))
-
-    document.dispatchEvent(createPageChangeEvent("shopping_cart"));
-}
-
-/**
- * Go to shopping cart with product
- * @param productId JSON string of product object
- */
-let goToShoppingCartWithProduct = (productId) => {
-    console.log("Trying to shopping cart with product id: ", productId)
-
-    ProductState.fetchProducts({product_id: productId}).then((res) => {
-        if (!res.ok) {
-            throw new Error(`Server responded with status: ${res.status}`);
-        }
-        return res.json();
-    }).then(products => {
-        if (products.length === 0) {
-            console.error("No product found with id: ", productId)
-            return null
-        }
-        let productForCart = products[0]
-        productForCart['quantity'] = 1 // default quantity
-        console.log("Going to shopping cart with product: ", products[0])
-        CartState.setItemToAdd(productForCart)
-        switchPage('shopping_cart')
-    })
-
-}
 
 const putProds = (productsDiv, products) => {
     productsDiv.innerText = ""
@@ -273,7 +115,7 @@ const loadComponents = () => {
         console.error("No products div found")
     }
     const stateProducts = ProductState.getAllProducts()
-    if (stateProducts.length === 0){
+    if (stateProducts.length === 0) {
         console.log("Loading products from server")
         fetch("http://localhost:900/product", {
             method: "GET",
@@ -286,7 +128,7 @@ const loadComponents = () => {
                 throw new Error(`Server responded with status: ${res.status}`);
             }
             return res.json();
-        }).then(products =>{
+        }).then(products => {
             ProductState.setAllProducts(products)
             putProds(productsDiv, products)
         })
@@ -325,10 +167,10 @@ const loadNavbarAuth = () => {
             About
         </p>
         <p onclick="switchPage('account')">
-        Account
+            Account
         </p>
         <p onclick="logout()">
-        Logout
+            Logout
         </p>
     <p onclick="switchPage('shopping_cart')">
         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 24 24">
@@ -366,7 +208,7 @@ const loadAccountPage = () => {
     }
     const token = localStorage.getItem("token")
     //todo optimize
-    const products = ProductState.fetchProducts({seller_id:user.user_uuid}, token)
+    const products = ProductState.fetchProducts({seller_id: user.user_uuid}, token)
 
 
     if (user) {
@@ -396,12 +238,10 @@ const loadAccountPage = () => {
                 throw new Error(`Server responded with status: ${res.status}`);
             }
             return res.json();
-        } ).then(products => {
+        }).then(products => {
             putProds(accountProductsDiv, products)
-        } )
+        })
     }
-
-
 
 
     // understand how to show both orders and products
@@ -540,6 +380,5 @@ window.switchPage = switchPage;
 window.register = register;
 window.login = login;
 window.logout = logout;
-window.goToShoppingCartWithProduct = goToShoppingCartWithProduct;
-window.removeProdFromCart = removeProdFromCart;
-window.modifyQuantity = modifyQuantity;
+
+window.createPageChangeEvent = createPageChangeEvent;
---
+++ frontend/scripts/stock.js
@@ -0,0 +1,168 @@
+
+// todo check if correct and add quantity
+let loadCartPage = () => {
+    let cartItems = CartState.getCartItems() // at least always []
+    const itemToAdd = CartState.getItemToAdd()
+
+    const storedCartItems = JSON.parse(localStorage.getItem("cartItems"))
+
+    if (storedCartItems && cartItems.length === 0 && !itemToAdd)
+        cartItems.push(...storedCartItems)
+
+    if (itemToAdd) {
+        // add the item to the cart
+        console.log("Adding item to cart: ", itemToAdd)
+        // check if the item is already in the cart
+        const existingItemIndex = cartItems.findIndex(item => item.product_id === itemToAdd.product_id)
+        if (existingItemIndex !== -1) {
+            // if it is, increase the quantity
+            if (cartItems[existingItemIndex].quantity + itemToAdd.quantity > itemToAdd.stock_count) {
+                console.warn("Cannot add more than stock count. Max is: " + itemToAdd.stock_count)
+            } else {
+                cartItems[existingItemIndex].quantity += itemToAdd.quantity
+            }
+        } else {
+            cartItems.push(itemToAdd)
+        }
+
+        CartState.setCartItems(cartItems)
+        localStorage.setItem("cartItems", JSON.stringify(cartItems))
+        CartState.setItemToAdd(null) // reset item to add
+    }
+
+    // getting them from local storage
+
+    console.log("Loading cart items from local storage", storedCartItems)
+
+    const cartDiv = document.getElementById("cart-items")
+
+
+    if (cartItems.length === 0) {
+        cartDiv.innerHTML = `
+        <div class="cart-empty">
+            <p>
+                Your cart is empty
+            </p>
+        </div>
+        `
+    }
+
+    for (let prod of cartItems) {
+        if (!prod || !prod.product_id) {
+            continue
+        }
+        cartDiv.innerHTML += `
+           <div class="cart-item">
+                <div id="cart-image">
+                    <img src="http://localhost:900/images?product_id=${prod.product_id}" alt="">
+                </div>
+                <div class="cart-info">
+                    <div class="cart-title">
+                         ${prod.name}
+                    </div>
+                    <div class="cart-price">
+                        $${prod.price}
+                    </div>
+                    <div onclick="removeProdFromCart(${prod.product_id})" class="cart-remove">
+                        Remove
+                    </div>
+                </div>
+                <div class="cart-quantity">
+                    <div class="quantity-minus" onclick="modifyQuantity(${prod.product_id}, -1, ${prod.stock_count})">
+                     -
+                    </div>
+                    <p>
+                        ${prod.quantity}
+                    </p>
+                    <div class="quantity-plus" onclick="modifyQuantity(${prod.product_id}, 1, ${prod.stock_count})">
+                     +
+                    </div>
+                </div>
+        </div>
+        `
+    }
+}
+
+
+
+let modifyQuantity = (productId, quantity, maxQuantity) => {
+    let cartItems = localStorage.getItem("cartItems")
+    if (!cartItems) {
+        console.error("No cart items found in local storage")
+        return
+    }
+
+    cartItems = JSON.parse(cartItems)
+    const itemIndex = cartItems.findIndex(item => item.product_id === productId)
+
+    if (itemIndex === -1) {
+        console.error("Product not found in cart")
+        return
+    }
+
+    // Modify the quantity
+    if (quantity < 0 && cartItems[itemIndex].quantity <= 1) {
+        console.warn("Cannot reduce quantity below 1")
+        return
+    }
+    // ensure maximum quantity is not exceeded. Max is product stock count.
+
+    if (quantity > 0 && cartItems[itemIndex].quantity >= maxQuantity) {
+        console.warn("Cannot increase quantity above " + maxQuantity)
+        return
+    }
+
+    cartItems[itemIndex].quantity += quantity
+
+    CartState.setCartItems(cartItems)
+    localStorage.setItem("cartItems", JSON.stringify(cartItems))
+
+    document.dispatchEvent(createPageChangeEvent("shopping_cart"));
+}
+
+
+let removeProdFromCart = (productId) => {
+    let cartItems = localStorage.getItem("cartItems")
+    if (!cartItems) {
+        console.error("No cart items found in local storage")
+        return
+    }
+
+    cartItems = JSON.parse(cartItems)
+    const newCartItems = cartItems.filter(item => item.product_id !== productId)
+    CartState.setCartItems(newCartItems)
+    localStorage.setItem("cartItems", JSON.stringify(newCartItems))
+
+    document.dispatchEvent(createPageChangeEvent("shopping_cart"));
+}
+
+/**
+ * Go to shopping cart with product
+ * @param productId JSON string of product object
+ */
+let goToShoppingCartWithProduct = (productId) => {
+    console.log("Trying to shopping cart with product id: ", productId)
+
+    ProductState.fetchProducts({product_id: productId}).then((res) => {
+        if (!res.ok) {
+            throw new Error(`Server responded with status: ${res.status}`);
+        }
+        return res.json();
+    }).then(products => {
+        if (products.length === 0) {
+            console.error("No product found with id: ", productId)
+            return null
+        }
+        let productForCart = products[0]
+        productForCart['quantity'] = 1 // default quantity
+        console.log("Going to shopping cart with product: ", products[0])
+        CartState.setItemToAdd(productForCart)
+        document.dispatchEvent(createPageChangeEvent("shopping_cart"));
+    })
+
+}
+
+window.loadCartPage = loadCartPage;
+window.modifyQuantity = modifyQuantity;
+window.removeProdFromCart = removeProdFromCart;
+window.goToShoppingCartWithProduct = goToShoppingCartWithProduct;
