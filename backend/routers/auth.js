22e24cd64f73b69f4cca6c87d118ea0deccc8352
Paolo_4 <paolo.ricci@devais.it>
2025-06-20 11:08:14 +0200

    fixes

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -2,86 +2,86 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import uuid4 from "uuid4";
-import {db} from "../lib/server/server.js";
-import User from "../lib/models/User.js";
+import { db } from "../lib/server/server.js";
+import User from "../lib/models/user.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // JWT Token generator
 const generateToken = (id) => {
-    return jwt.sign({user_uuid: id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+  return jwt.sign({ user_uuid: id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 };
 
 router.post("/login", async (req, res) => {
-    const {email, password} = req.body;
-    try {
-        const user = await User.getUserByEmail(email)
-
-        if (user === null) {
-            return res.status(401).json({message: "Invalid credentials"});
-        }
-
-        const isMatch = User.checkPassword(password, user.password);
-        if (!isMatch) {
-            return res.status(401).json({message: "Invalid credentials"});
-        }
-
-        const token = generateToken(user.user_uuid);
-        res.json({token, user});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({message: "Server error"});
-    }
-})
-
-router.post("/register", async (req, res) => {
-    const {name, lastname, email, password, role_id} = req.body;
-    const hashedPassword = User.hashPassword(password)
-
-    try {
-        const user = {
-            name,
-            lastname,
-            email,
-            hashedPassword,
-            role_id
-        }
-        const userUUid = await User.newUser(user)
-        res.json({token: generateToken(userUUid), user: user});
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({message: "User already exists"});
-    }
-})
-
-router.get("/token/validate", (req, res) => {
-    const token = req.headers.authorization;
-    if (!token || !token.startsWith("Bearer ")) {
-        //return res.status(401).send("Unauthorized");
-        res.json({valid: false});
-    }
-
-    jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            //return res.status(403).send("Forbidden");
-            res.json({valid: false});
-        }
-        res.json({valid: true});
-    });
-})
-
-router.get("/user", async (req, res) => {
-    const user_uuid = req.user_uuid
-    console.log(user_uuid)
-    const user = await User.getUserById(user_uuid)
+  const { email, password } = req.body;
+  try {
+    const user = await User.getUserByEmail(email);
 
     if (user === null) {
-        return res.status(404).json({message: "User not found"});
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    res.json(user);
+    const isMatch = User.checkPassword(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
 
-})
+    const token = generateToken(user.user_uuid);
+    res.json({ token, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
+router.post("/register", async (req, res) => {
+  const { name, lastname, email, password, role_id } = req.body;
+  const hashedPassword = User.hashPassword(password);
+
+  try {
+    const user = {
+      name,
+      lastname,
+      email,
+      hashedPassword,
+      role_id,
+    };
+    const userUUid = await User.newUser(user);
+    res.json({ token: generateToken(userUUid), user: user });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "User already exists" });
+  }
+});
+
+router.get("/token/validate", (req, res) => {
+  const token = req.headers.authorization;
+  if (!token || !token.startsWith("Bearer ")) {
+    //return res.status(401).send("Unauthorized");
+    res.json({ valid: false });
+  }
+
+  jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      //return res.status(403).send("Forbidden");
+      res.json({ valid: false });
+    }
+    res.json({ valid: true });
+  });
+});
+
+router.get("/user", async (req, res) => {
+  const user_uuid = req.user_uuid;
+  console.log(user_uuid);
+  const user = await User.getUserById(user_uuid);
+
+  if (user === null) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.json(user);
+});
 
 export default router;
